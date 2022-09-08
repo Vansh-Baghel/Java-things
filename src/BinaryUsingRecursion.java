ce69f163735b46c4eeb25d4a0a766026647ae377
Vansh-Baghel <vanshbaghel912@gmail.com>
2022-09-08 01:52:38 -0700

    Solved Binary using recursion

---
+++ src/BinaryUsingRecursion.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class BinaryUsingRecursion {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        int ans = search(nums , target , 0 , nums.length - 1);
+        System.out.print(ans);
+    }
+
+    static int search(int[] nums, int target, int start , int end) {
+        while (start < end){
+            int mid = start + (end - start) / 2;
+
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if (target < nums[mid]){
+                return search(nums , target , start , mid - 1);
+            }
+            else if (target > nums[mid]){
+                return search(nums , target , mid + 1 , end);
+            }
+
+        }
+        return -1;
+    }
+}
