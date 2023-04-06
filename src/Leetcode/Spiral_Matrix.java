4565d0e35d3c6655394ac8c98165aea00d8d6c04
vansh <vanshbaghel912@gmail.com>
2023-04-06 16:45:17 +0530

    Added

---
+++ src/Leetcode/Spiral_Matrix.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        // d is direction
+        int d = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int top = 0;
+        int bottom = m - 1;
+        int left = 0;
+        int right = n - 1;
+
+        // While condition is such so that values dont repeat of overlap within a loop.
+        while (top <= bottom && left <= right){
+            // If any of these if conditions become true, it would go to last step where d = (d+ 1) % 4 is there where we are increasing the value of d.
+
+            // Left to right
+            if (d == 0){
+                for (int i = left; i <= right; i++) {
+                    list.add(matrix[top][i]);
+                }
+                top++;
+            }
+            // Top to Bottom
+            else if (d == 1){
+                for (int i = top; i <= bottom; i++) {
+                    list.add(matrix[i][right]);
+                }
+                right--;
+            }
+            //  Right to Left
+            else if (d == 2){
+                for (int i = right; i >= left; i--) {
+                    list.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            // Bottom to Top
+            else if (d == 3){
+                for (int i = bottom; i >= top; i--) {
+                    list.add(matrix[i][left]);
+                }
+                left++;
+            }
+            d=(d+1)%4;
+        }
+        return list;
+    }
+}
--- src/Leetcode/Spiral_Matrix_IV.java
+++ src/Leetcode/Spiral_Matrix_IV.java
@@ -12,6 +12,7 @@ public class Spiral_Matrix_IV {
 
     public int[][] spiralMatrix(int m, int n, ListNode head) {
         int[][] mat = new int[m][n];
+        // Setting all the blocks of mat as -1, so when the head becomes null and if any blocks are left, then we can get -1 as its value.
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 mat[i][j] = -1;
@@ -23,27 +24,33 @@ public class Spiral_Matrix_IV {
         int right = n - 1;
 
         while (head != null){
-                for (int i = left; i <= right && head != null; i++) {
-                    mat[top][i] = head.val;
-                    head = head.next;
-                }
-                top++;
-                for (int i = top; i <= bottom && head != null; i++) {
-                    mat[i][right] = head.val;
-                    head = head.next;
-                }
-                right--;
-                for (int i = right; i >= left && head != null; i--) {
-                    mat[bottom][i] = head.val;
-                    head = head.next;
+            // printing from left to right
+            for (int i = left; i <= right && head != null; i++) {
+                mat[top][i] = head.val;
+                head = head.next;
             }
-                bottom--;
-                for (int i = bottom; i >= top && head != null; i--) {
-                    mat[i][left] = head.val;
-                    head = head.next;
+            // Increasing top value as we already printed the top row. So next time it starts from new line.
+            top++;
+            // printing from top to bottom
+            for (int i = top; i <= bottom && head != null; i++) {
+                mat[i][right] = head.val;
+                head = head.next;
             }
-                left++;
+            right--;
+            // printing from right to left
+            for (int i = right; i >= left && head != null; i--) {
+                mat[bottom][i] = head.val;
+                head = head.next;
+            }
+            bottom--;
+            // printing from bottom to top
+            for (int i = bottom; i >= top && head != null; i--) {
+                mat[i][left] = head.val;
+                head = head.next;
+            }
+            left++;
         }
+
         return mat;
     }
 }
