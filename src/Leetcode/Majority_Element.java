60e7c2a61fbfaf86fd9536b1670e77c5d946e272
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-04-21 20:47:16 +0530

    added files

--- src/Leetcode/Majority_Element.java
+++ src/Leetcode/Majority_Element.java
@@ -7,19 +7,38 @@ public class Majority_Element {
     }
 
     static int majorityElement(int[] nums) {
-        int el = 0;
+        // Most optimized approach as it doesn't take space.
+        int maxElement = 0;
         int count = 0;
-        for(int num: nums){
-            if (count == 0){
-                el = num;
-            }
-            if (num == el){
-                ++count;
-            }
-            else{
-                --count;
-            }
+        for (int num: nums){
+            // If the count = 0, means it has nullified the max element with other elements.
+            if (count == 0) maxElement = num;
+
+            // We are following moore's value algorithm where the max count would nullify the other numbers and in the end whichever element is left, we can take it as majority.
+            if (num == maxElement){
+                count++;
+            } else count--;
         }
-        return el;
+        return maxElement;
+
+        // With hashmap
+//        HashMap<Integer, Integer> hm = new HashMap<>();
+//        int max = 0;
+//
+//        for (int num: nums){
+//            if (hm.containsKey(num)){
+//                hm.put(num, hm.get(num) + 1);
+//            }
+//            else {
+//                hm.put(num, 1);
+//            }
+//        }
+//
+//        for (int num: nums){
+//           max = Math.max(max, hm.get(num));
+//        }
+//
+//        // max would return the number of times maxElement has occured. You could return the key using the value to get the output.
+//        return max;
     }
 }
